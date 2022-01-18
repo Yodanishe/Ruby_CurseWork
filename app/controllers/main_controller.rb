d7d01f0609e37ccb69489fa028f6d551eb9594b3
Yodanishe <fire.demon@mail.ru>
2022-01-19 00:18:13 +0700

    update controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,15 @@
 class MainController < ApplicationController
+
+  before_action :set_page_options
+
   def index
     @developers = Developer.limit(3)
     @hits = Product.all.limit(8)
+  end
+
+  def set_page_options
+    @page_title="SIBGUCHI GAMESHOP"
+    @page_description="Games"
+    @page_keywords="Shooter Adventure Horror Sport"
   end
 end
