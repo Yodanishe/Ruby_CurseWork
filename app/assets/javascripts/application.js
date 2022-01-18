b04d99799e62d938db5fdbda136d9c4a22b584cb
Yodanishe <fire.demon@mail.ru>
2022-01-19 00:16:52 +0700

    update all applications

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,14 @@
+//  =require rails-ujs
+//  =require jquery-1.11.0.min
+//  =require bootstrap.min
+//  =require typeahead.bundle
+//  =require jquery.easydropdown
+//  =require easydrop
+//  =require memenu
+//  =require mem
+//  =require responsiveslides.min
+//  =require responsive
+//  =require imagezoom
+//  =require jquery.flexslider
+//  =require flexs
+
