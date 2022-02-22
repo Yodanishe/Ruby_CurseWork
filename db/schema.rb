9473c4314430a51494337ed07dbba9ca377d29c2
Yodanishe <fire.demon@mail.ru>
2022-02-22 14:20:44 +0700

    fix database access

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,29 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_201_173_756) do
+ActiveRecord::Schema.define(version: 2022_02_01_173756) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'developers', force: :cascade do |t|
-    t.string 'title'
-    t.string 'description'
-    t.string 'image'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "developers", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'title'
-    t.string 'description'
-    t.string 'image'
-    t.float 'price'
-    t.string 'status'
-    t.bigint 'developer_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['developer_id'], name: 'index_products_on_developer_id'
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "image"
+    t.float "price"
+    t.string "status"
+    t.bigint "developer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["developer_id"], name: "index_products_on_developer_id"
   end
 
-  add_foreign_key 'products', 'developers'
+  add_foreign_key "products", "developers"
 end
