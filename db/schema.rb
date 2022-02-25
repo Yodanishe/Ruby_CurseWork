eedcdad7b21706f8784f2dd18a6e8ad1ac3aa2e8
Kaply9l <kislov.igor5@icloud.com>
2022-02-25 16:27:51 +0700

    add models user, cart, item

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_173756) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_25_083800) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id"
+    t.integer "product_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "developers", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "image"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -30,9 +44,15 @@ ActiveRecord::Schema.define(version: 2022_02_01_173756) do
     t.float "price"
     t.string "status"
     t.bigint "developer_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["developer_id"], name: "index_products_on_developer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "products", "developers"
