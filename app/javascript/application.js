5341f49894401016a38831ce1d501586342c866f
Yodanishe <fire.demon@mail.ru>
2022-02-26 15:21:08 +0700

    fix account box

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
-//= require jquery
-//= require jquery_ujs
-//  =require rails-ujs
-//  =require jquery-1.11.0.min
-//  =require bootstrap.min
-//  =require typeahead.bundle
-//  =require jquery.easydropdown
-//  =require easydrop
-//  =require memenu
-//  =require mem
-//  =require responsiveslides.min
-//  =require responsive
-//  =require imagezoom
-//  =require jquery.flexslider
-//  =require flexs
+import "Ruby_CurseWork/shop/app/javascript/controllers"
